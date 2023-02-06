fcbef9287c4aef0da3bcdc2d2a83392bf2be031f
Jeferson Coutinho <jefissuu@gmail.com>
2023-02-06 03:01:58 -0300

    add transparent status bar

--- app/src/main/java/com/jefisu/diary/MainActivity.kt
+++ app/src/main/java/com/jefisu/diary/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.jefisu.diary.features_auth.presentation.NavGraphs
 import com.jefisu.diary.ui.theme.DiaryTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
@@ -19,10 +22,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             DiaryTheme(dynamicColor = false) {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    DestinationsNavHost(navGraph = NavGraphs.root)
+                    DestinationsNavHost(
+                        navGraph = NavGraphs.root,
+                        modifier = Modifier
+                            .statusBarsPadding()
+                            .navigationBarsPadding()
+                    )
                 }
             }
         }
