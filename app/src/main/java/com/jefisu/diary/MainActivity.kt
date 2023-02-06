ff2578bdcc6bf80d088e4f6aeeb725e01ccaa0ef
Jeferson Coutinho <jefissuu@gmail.com>
2023-02-06 02:20:38 -0300

    add splash screen

--- app/src/main/java/com/jefisu/diary/MainActivity.kt
+++ app/src/main/java/com/jefisu/diary/MainActivity.kt
@@ -3,11 +3,13 @@ package com.jefisu.diary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jefisu.diary.ui.theme.DiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryTheme(dynamicColor = false) {
             }
