70d1a178c4b98370efb0607ccd57d3a36803a0d5
Jeferson Coutinho <jefissuu@gmail.com>
2023-02-08 23:09:21 -0300

    Adjust content in diary with horizontal screen

--- app/src/main/java/com/jefisu/diary/features_diary/presentation/DiaryScreen.kt
+++ app/src/main/java/com/jefisu/diary/features_diary/presentation/DiaryScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -33,6 +35,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -119,7 +122,12 @@ fun DiaryScreen(
             LazyColumn(
                 modifier = Modifier
                     .padding(horizontal = 24.dp)
-                    .padding(top = paddingValues.calculateTopPadding())
+                    .padding(
+                        top = paddingValues.calculateTopPadding(),
+                        bottom = paddingValues.calculateBottomPadding(),
+                        start = paddingValues.calculateStartPadding(LayoutDirection.Ltr),
+                        end = paddingValues.calculateEndPadding(LayoutDirection.Rtl)
+                    )
             ) {
                 state.diaries.forEach { (localDate, diaries) ->
                     stickyHeader {
